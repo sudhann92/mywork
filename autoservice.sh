f5897481634ac506bc7488914443a204f2da2dce
sudhann92 <50201866+sudhann92@users.noreply.github.com>
2019-05-03 17:28:08 +0530

    Add files via upload

---
+++ autoservice.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+service=`ps -ef | grep aex | grep -v grep | grep -v $0|wc -l`
+
+if [ $service -gt 0 ]
+then
+
+echo "$service is running" > /dev/null
+else
+
+systemctl start altiris
+
+fi
+exit 
