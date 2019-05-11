a1149d0aa18d4af4827ef9229f93f8f59edf4da3
sudhann92 <50201866+sudhann92@users.noreply.github.com>
2019-05-11 16:49:26 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -18,8 +18,14 @@ tags {
   subnet_id = "subnet-953f8dbb"
   associate_public_ip_address = "true"
   vpc_security_group_ids = ["sg-08222a038c4ccc92a"]
-  user_data ="#!/bin/bash\nyum update -y\nyum install httpd -y\nsystemctl enable httpd\nsystemctl start httpd\n"
+    user_data = <<EOF
+#!/bin/bash
+yum update -y
+yum install httpd -y
+systemctl enable httpd
+systemctl start httpd
 
+EOF
 
 }
 
