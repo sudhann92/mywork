13ac5963abed23819b378e8ae1830f099093e6db
sudhann92 <50201866+sudhann92@users.noreply.github.com>
2019-05-11 15:12:49 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,19 +6,20 @@ provider "aws" {
 
 }
 
-resource "aws_instance" "my_test"{
+resource "aws_instance" "my-instance" {
   ami = "ami-0015b9ef68c77328d"
   instance_type = "t2.micro"
 tags {
 
-    Name = "test-instance"
-
+    Name = "terraform1"
   }
 
   key_name = "keypair"
-
   subnet_id = "subnet-953f8dbb"
   associate_public_ip_address = "true"
+  vpc_security_group_ids = ["sg-08222a038c4ccc92a"]
+  user_data ="#!/bin/bash\nyum update -y\nyum install httpd -y\nsystemctl enable httpd\nsystemctl start httpd\n"
+
 
 }
 
